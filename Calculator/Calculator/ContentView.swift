bb58de307dd920ea91339f756666556e56d9772a
서민주 <minju1307@gmail.com>
2020-07-30 17:11:09 +0900

    Refactor calculator view

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -5,45 +5,92 @@
 //  Created by ho-seong yeom on 2020/07/29.
 //  Copyright © 2020 Nathan. All rights reserved.
 //
- 
+
 import SwiftUI
 
-struct ContentView: View {
+enum CalculatorButton: String {
+    case zero, one, two, three, four, five, six, seven, eight, nine
+    case equals, plus, minus, multiply, divide
+    case ac, plusMinus, percent, dot
     
-    let buttons = [
-        
-        ["7", "8", "9", "X"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "+"],
-        ["0", ".", ".", "="]
+    var title: String {
+        switch self {
+        case .zero: return "0"
+        case .one: return "1"
+        case .two: return "2"
+        case .three: return "3"
+        case .four: return "4"
+        case .five: return "5"
+        case .six: return "6"
+        case .seven: return "7"
+        case .eight: return "8"
+        case .nine: return "9"
+            
+        case .equals: return "="
+        case .plus: return "+"
+        case .minus: return "-"
+        case .multiply: return "X"
+        case .divide: return "/"
+        case .plusMinus: return "±"
+        case .percent: return "%"
+        case .dot: return "."
+            
+        default: return "AC"
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .dot:
+            return Color(.darkGray)
+        case .ac, .plusMinus, .percent:
+            return Color(.lightGray)
+        default:
+            return .orange
+        }
+    }
+}
+
+struct ContentView: View {
+   
+    let buttons: [[CalculatorButton]] = [
+        [.ac, .plusMinus, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .dot, .dot, .equals]
     ]
     
     var body: some View {
         
         ZStack (alignment: .bottom) {
             Color.black.edgesIgnoringSafeArea(.all)
-
-        
-        VStack {
             
-            HStack {
-                Spacer()
-                Text("42").foregroundColor(.white)
-                    .font(.system(size: 64))
+            VStack(spacing: 12) {
                 
-            }.padding()
-            
-            
-            ForEach(buttons, id: \.self) { row in
-                HStack (spacing: 12 ) {
-                    ForEach(row, id: \.self) { button in
-                        Text(button)
-                            .font(.system(size: 32))
-                            .frame(width: self.buttonWidth(), height: self.buttonWidth())
-                                .foregroundColor(.white)
-                                .background(Color.yellow)
-                            .cornerRadius(self.buttonWidth())
+                HStack {
+                    Spacer()
+                    Text("42").foregroundColor(.white)
+                        .font(.system(size: 64))
+                        .frame(width: 80, height: 80)
+                }.padding()
+                
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack (spacing: 12) {
+                        ForEach(row, id: \.self) { button in
+                            
+                            Button(action: {
                                 
+                            }) {
+                                Text(button.title)
+                                .font(.system(size: 32))
+                                    .frame(width: self.buttonWidth(button: button), height: self.buttonWidth(button: button))
+                                .foregroundColor(.white)
+                                .background(button.backgroundColor)
+                                    .cornerRadius(self.buttonWidth(button: button))
+                            }
+                            
                         }
                     }
                 }
@@ -51,8 +98,7 @@ struct ContentView: View {
         }
     }
     
-    
-    func buttonWidth() -> CGFloat {
+    func buttonWidth(button: CalculatorButton) -> CGFloat {
         return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
@@ -60,6 +106,11 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ForEach(["iPhone 11 Pro", "iPhone 8"], id: \.self) { deviceName in
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+        
     }
 }
